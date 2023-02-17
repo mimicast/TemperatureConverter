9f3482aeb2b5b234aeb81b88ad516ac2eb781a46
mimicast <47404393+mimicast@users.noreply.github.com>
2023-02-17 17:00:48 -0600

    Create tempConverter.py

---
+++ tempConverter.py
@@ -0,0 +1,7 @@
+print("Welcome to the Cº to Fº Converter!")
+print("This code will convert your temperature in Celsius into Farenheit.")
+
+currCelsius = float(input("What's your temperature in Celsius?: "))
+currFarenheit = (currCelsius*1.8)+32
+
+print(f"The temperature in Farenheit is {currFarenheit}.")
